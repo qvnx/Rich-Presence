bee7c10c1211c8996fc79bd01b579aae5dd657fc
Qynx <abqynx@gmail.com>
2021-01-16 15:38:47 -0500

    Rewrote Index
    Lots of new improvements were added to the script such as:
     -  New output format
     -  New state compatibility
     -  Improved compatibility with other objects
    
    Co-Authored-By: Yukii <45071533+PrincessAkira@users.noreply.github.com>
    Co-Authored-By: Vanished <vanished.apps@gmail.com>

--- index.js
+++ index.js
@@ -10,53 +10,46 @@ const spacer = `   `;
 
 // Startup Output
 console.log("Starting up ..");
-console.log(spacer + "Gathering Login ..");
-client.login({
-   clientId: config.clientID
-}).catch(console.error);
-console.log(spacer + spacer + "Logged in!");
+console.log(spacer + "Gathering Login & Authentication ..");
 
-console.log(spacer + "Client ..");
 // On-ready Function
 client.on('ready', () => {
-   console.log(spacer + spacer + "Ready");
+   console.log(spacer + spacer + "Authorized via", client.user.username + '#' + client.user.discriminator);
    console.log(spacer + "Presence ..");
-   client.request('SET_ACTIVITY', {
-      pid: process.pid,
-      activity: {
-         details: config.details,
-         state: config.state,
-         assets: {
-            large_image: config.largeImage.image,
-            large_text: config.largeImage.text,
-            small_image: config.smallImage.image,
-            small_text: config.smallImage.text,
-         },
-         buttons: [{
-               label: config.button1.text,
-               url: config.button1.url
-            },
-            {
-               label: config.button2.text,
-               url: config.button2.url
-            }
-         ]
-      }
-   })
-   console.log(spacer + spacer + "*  Loaded [/presence]");
-   console.log(spacer + spacer + "*  Loaded [/assets]");
-   console.log(spacer + spacer + "*  Loaded [/interaction]");
+   client.request("SET_ACTIVITY", {
+			pid: process.pid,
+			activity: {
+				// Client Config
+				details: config.description,
+				state: config.state,
+				assets: {
+					large_image: config.largeImage.image,
+					large_text: config.largeImage.tooltip,
+					small_image: config.smallImage.image,
+					small_text: config.smallImage.tooltip,
+				},
+				// Button Config
+				buttons: [
+					{
+						label: config.button1.text,
+						url: config.button1.redirect,
+					},
+					{
+						label: config.button2.text,
+						url: config.button2.redirect,
+					},
+				],
+			},
+		});
 	console.log(spacer + spacer + "*  Loaded [/presence]"); // Presence
 	console.log(spacer + spacer + "*  Loaded [/assets]"); // Assets
 	console.log(spacer + spacer + "*  Loaded [/short-description]"); // Details
 	console.log(spacer + spacer + "*  Loaded [/short-state]"); // State
    console.log(spacer + spacer + "*  Loaded [/buttons]"); // Buttons
    console.log(spacer + spacer + "Presence Ready");
-   console.log(spacer);
-   console.log(spacer);
-   console.log(spacer);
-   console.log(spacer);
-   console.log(spacer);
 });
+
 // Starts up the presence
+client.login(
+   { clientId: config.clientID }
 ).catch(console.error); // Catches any errors and outputs them
