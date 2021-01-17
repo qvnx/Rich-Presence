21d2ff0e924b93e1e863b4a1c166011b1f28227c
Yukii <broxmodz@gmail.com>
2021-01-17 14:39:57 +0100

    added button on and off fuckery. JS cant use fucking booleans because everything defaults to true unless is specify with var number wtf JS

--- index.js
+++ index.js
@@ -1,44 +1,91 @@
 // Imports
-const rpc = require(`discord-rpc`);
-const config = require(`./config.json`);
-const client = new rpc.Client({transport: 'ipc'})
+const rpc = require('discord-rpc');
+const config = require('./config.json');
+const client = new rpc.Client({
+	transport: 'ipc'
+})
 
-// Presets
+// Console Presets
 const spacer = `   `;
 
+// Enabled / Disabled
+
+Buttons = Boolean(config.buttons);
+if (config.buttons == 'false') {
+	var x = 0
+} else if (config.buttons == 'true') {
+	var x = 1
+}
+
+// External Stuff
+Timestamp = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+
+// Startup Output
 console.log("Starting up ..");
+console.log(spacer + "Gathering Login & Authentication ..");
+console.log(Buttons.toString());
 
-console.log(spacer + "Gathering Login ..");
-client.login({ clientId: config.clientID }).catch(console.error);
-console.log(spacer + spacer + "Logged in!");
-
-console.log(spacer + "Client ..");
+// On-ready Function
 client.on('ready', () => {
-   console.log(spacer + spacer + "Ready");
-   console.log(spacer + "Presence ..");
-   client.request('SET_ACTIVITY', {
-      pid: process.pid,
-      activity: {
-         details: config.details,
-         assets: {
-            large_image: config.largeImage.image,
-            large_text: config.largeImage.text,
-            small_image: config.smallImage.image,
-            smal_text: config.smallImage.text
-         },
-         buttons: [
-            { label: config.button1.text, url: config.button1.url },
-            { label: config.button2.text, url: config.button2.url }
-         ]
-      }
-   })
-   console.log(spacer + spacer + "*  Loaded [/presence]");
-   console.log(spacer + spacer + "*  Loaded [/assets]");
-   console.log(spacer + spacer + "*  Loaded [/interaction]");
-   console.log(spacer + spacer + "Presence Ready");
-   console.log(spacer);
-   console.log(spacer);
-   console.log(spacer);
-   console.log(spacer);
-   console.log(spacer);
+	console.log(spacer + spacer + "Authorized via", client.user.username + '#' + client.user.discriminator);
+	console.log(spacer + "Presence ..");
+
+	if (x === 0) {
+		client.clearActivity;
+		client.request("SET_ACTIVITY", {
+			pid: process.pid,
+			activity: {
+				// Client Config
+				details: config.description,
+				state: config.state,
+				assets: {
+					large_image: config.largeImage.image,
+					large_text: config.largeImage.tooltip,
+					small_image: config.smallImage.image,
+					small_text: config.smallImage.tooltip,
+				},
+			},
+		});
+	} else if (x === 1) {
+		client.clearActivity;
+		client.request("SET_ACTIVITY", {
+			pid: process.pid,
+			activity: {
+				// Client Config
+				details: config.description,
+				state: config.state,
+				assets: {
+					large_image: config.largeImage.image,
+					large_text: config.largeImage.tooltip,
+					small_image: config.smallImage.image,
+					small_text: config.smallImage.tooltip,
+				},
+				// Button Config
+				buttons: [{
+						label: config.button1.text,
+						url: config.button1.redirect,
+					},
+					{
+						label: config.button2.text,
+						url: config.button2.redirect,
+					},
+				],
+			},
+		});
+	}
+	else {
+		console.log(spacer + spacer + "*  Please provide valid Arguments for Buttons e.g enabled or disabled");
+	}
+	console.log(spacer + spacer + "*  Loaded [/presence]"); // Presence
+	console.log(spacer + spacer + "*  Loaded [/assets]"); // Assets
+	console.log(spacer + spacer + "*  Loaded [/short-description]"); // Details
+	console.log(spacer + spacer + "*  Loaded [/short-state]"); // State
+	console.log(spacer + spacer + "*  Loaded [/buttons]"); // Buttons
+	console.log(spacer + spacer + "Presence Ready");
+	console.log(spacer + "*  Running since " + Timestamp); // Show when Started Program
 });
+
+// Starts up the presence
+client.login({
+	clientId: config.clientID
+}).catch(console.error); // Catches any errors and outputs them
